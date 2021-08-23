897419b4ed3b75de6c7ed09888cf389d37361cf0
Sam <oceansam101@gmail.com>
2021-08-23 19:04:51 -0400

    Frontend Polish

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
 	<div class="home">
-		<h1>Example Full Stack Usage</h1>
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<sample-component class="sample-comp" />
+		<div>
+			<h1>Example Full Stack Usage</h1>
+			<img
+				alt="Pepe dance"
+				src="https://c.tenor.com/a_W49uimPs8AAAAM/mer-an-ge-l-pepe.gif"
+			/>
+			<p>
+				Be sure to check out the
+				<a href="https://github.com/oceansam/vue-mongo-boilerplate">READ.ME</a>
+				for any information you may have missed!
+			</p>
+		</div>
 
+		<sample-component class="sample-comp" />
 		<button @click="reqBackend">Read Data</button>
 		<div v-if="display">
 			<p v-for="(data, index) in display.data" :key="index">
@@ -15,6 +25,9 @@
 <style lang="scss" scoped>
 .sample-comp {
 	margin-bottom: 50px;
+}
+a {
+	color: green;
 }
 </style>
 <script>
